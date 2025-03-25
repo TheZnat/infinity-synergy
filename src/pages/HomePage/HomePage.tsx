e7d55bba54b29a1bb34ae7d541b8c89ad4d01244
Max K <theZnat7@yandex.ru>
2025-03-26 01:56:37 +0300

    init

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Home Page</h1>
+      <p>This is the Home Page component.</p>
+    </div>
+  );
+};
+
+export default HomePage;
